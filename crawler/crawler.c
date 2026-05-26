32830ed6a9bce7d3d47f812bd3c4a3c7bfdeac0b
Alejandro000001 <alejandro.moroaraujo@gmail.com>
2026-05-26 10:51:05 -0400

    actual crawler skelly

---
+++ crawler/crawler.c
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bag.h"
+#include "hashtable.h"
+#include "webpage.h"
+#include "pagedir.h"
+#include "mem.h"
+
+static void parseArgs(const int argc, char* argv[],
+                      char** seedURL, char** pageDirectory, int* maxDepth);
+static void crawl(char* seedURL, char* pageDirectory, const int maxDepth);
+static void pageScan(webpage_t* page, bag_t* pagesToCrawl, hashtable_t* pagesSeen);
+
+int main(const int argc, char* argv[])
+{
+  char* seedURL = NULL;
+  char* pageDirectory = NULL;
+  int maxDepth = 0;
+
+  parseArgs(argc, argv, &seedURL, &pageDirectory, &maxDepth);
+  crawl(seedURL, pageDirectory, maxDepth);
+
+  return 0;
+}
+
+/* Parse and validate command-line arguments.
+ * Normalize seedURL and confirm it is internal.
+ * Initialize pageDirectory. Validate maxDepth range.
+ * Exit non-zero on any error.
+ */
+static void parseArgs(const int argc, char* argv[],
+                      char** seedURL, char** pageDirectory, int* maxDepth)
+{
+
+}
+
+/* Crawl from seedURL to maxDepth, saving pages in pageDirectory.
+ * Uses a bag of pages to crawl and a hashtable of seen URLs.
+ */
+static void crawl(char* seedURL, char* pageDirectory, const int maxDepth)
+{
+}
+
+/* Scan the given page for URLs. For each internal URL not yet seen,
+ * add it to both the hashtable and the bag.
+ */
+static void pageScan(webpage_t* page, bag_t* pagesToCrawl, hashtable_t* pagesSeen)
+{
+
+}
