00371c54fd793f32c0661c80673f788a111eca19
maxxl28 <maxliusoccer@gmail.com>
2026-05-27 20:24:07 -0400

    crawler: add parseArgs implementation

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -34,7 +34,40 @@ int main(const int argc, char* argv[])
 static void parseArgs(const int argc, char* argv[],
                       char** seedURL, char** pageDirectory, int* maxDepth)
 {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: ./crawler seedURL pageDirectory maxDepth\n");
+        exit(1);
+    }
 
+    // validate and normalize seedURL
+    *seedURL = normalizeURL(argv[1]);
+    if (*seedURL == NULL) {
+        fprintf(stderr, "Error: seedURL could not be normalized\n");
+        exit(1);
+    }
+    if (!isInternalURL(*seedURL)) {
+        fprintf(stderr, "Error: seedURL is not internal\n");
+        free(*seedURL);
+        exit(1);
+    }
+    // validate pageDirectory
+    *pageDirectory = argv[2];
+    if (!pagedir_init(*pageDirectory)) {
+        fprintf(stderr, "Error: cannot initialize pageDirectory: %s\n", *pageDirectory);
+        exit(1);
+    }
+
+    
+    // validate maxDepth
+    char excess;
+    if (sscanf(argv[3], "%d%c", maxDepth, &excess) != 1) {
+        fprintf(stderr, "Error: maxDepth must be an integer\n");
+        exit(1);
+    }
+    if (*maxDepth < 0 || *maxDepth > 10) {
+        fprintf(stderr, "Error: maxDepth must be between 0 and 10\n");
+        exit(1);
+    }
 }
 
 /* Crawl from seedURL to maxDepth, saving pages in pageDirectory.
