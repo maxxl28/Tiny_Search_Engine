bc77cd41a060f51792c768e9bf19739dd3d94166
Peter Owolabi <sp26peter@plink.cs.dartmouth.edu>
2026-06-01 21:33:03 -0400

    Common: Resolve conflict with final pagedir including all four functions

--- common/pagedir.c
+++ common/pagedir.c
@@ -1,3 +1,7 @@
+/*
+ * pagedir.c Max Peter Ale May 2026
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -6,32 +10,76 @@
 #include "pagedir.h"
 #include "../libcs50/file.h"
 
-bool 
+/* see pagedir.h for description */
+bool
 pagedir_init(const char* pageDirectory)
 {
-    // construct the pathname for the crawler
-    char filepath[strlen(pageDirectory) + strlen("/.crawler") + 1];
-    sprintf(filepath, "%s/.crawler", pageDirectory);
+  char filepath[strlen(pageDirectory) + strlen("/.crawler") + 1];
+  sprintf(filepath, "%s/.crawler", pageDirectory);
 
-    FILE* fp = fopen(filepath, "w");
-    if (fp == NULL) {
-        return false;
-    }
+  FILE* fp = fopen(filepath, "w");
+  if (fp == NULL) {
+    return false;
+  }
 
-    fclose(fp);
-    return true;
+  fclose(fp);
+  return true;
 }
 
-void 
+/* see pagedir.h for description */
+void
 pagedir_save(const webpage_t* page, const char* pageDirectory, const int docID)
 {
-    char filepath[strlen(pageDirectory) + 100];
-    sprintf(filepath, "%s/%d", pageDirectory, docID);
-    FILE* fp = fopen(filepath, "w");
-    if (fp == NULL) {
-        fprintf(stderr, "Cannot save page: %s\n", filepath);
-        exit(1);
-    }
-    fprintf(fp, "%s\n%d\n%s", webpage_getURL(page), webpage_getDepth(page), webpage_getHTML(page));
-    fclose(fp);
+  char filepath[strlen(pageDirectory) + 100];
+  sprintf(filepath, "%s/%d", pageDirectory, docID);
+
+  FILE* fp = fopen(filepath, "w");
+  if (fp == NULL) {
+    fprintf(stderr, "Cannot save page: %s\n", filepath);
+    exit(1);
+  }
+
+  fprintf(fp, "%s\n%d\n%s",
+          webpage_getURL(page),
+          webpage_getDepth(page),
+          webpage_getHTML(page));
+  fclose(fp);
+}
+
+/* see pagedir.h for description */
+bool
+pagedir_validate(const char* pageDirectory)
+{
+  char filepath[strlen(pageDirectory) + strlen("/.crawler") + 1];
+  sprintf(filepath, "%s/.crawler", pageDirectory);
+
+  FILE* fp = fopen(filepath, "r");
+  if (fp == NULL) {
+    return false;
+  }
+
+  fclose(fp);
+  return true;
+}
+
+/* see pagedir.h for description */
+webpage_t*
+pagedir_load(const char* pageDirectory, const int docID)
+{
+  char filepath[strlen(pageDirectory) + 20];
+  sprintf(filepath, "%s/%d", pageDirectory, docID);
+
+  FILE* fp = fopen(filepath, "r");
+  if (fp == NULL) {
+    return NULL;
+  }
+
+  char* url = file_readLine(fp);
+  char depthStr[20];
+  fgets(depthStr, 20, fp);
+  int depth = atoi(depthStr);
+  char* html = file_readFile(fp);
+
+  fclose(fp);
+  return webpage_new(url, depth, html);
 }
--- common/pagedir.h
+++ common/pagedir.h
@@ -4,20 +4,29 @@
 #include <stdbool.h>
 #include "../libcs50/webpage.h"
 
-/**************** pagedir_init ****************/
-/* Mark a directory as a Crawler-produced directory by creating a file named .crawler inside it.
+/* Mark a directory as a Crawler-produced directory.
  * Caller provides: a valid pathname string for pageDirectory
- * We return: true if successful and false otherwise
+ * We return: true if successful, false otherwise
  */
 bool pagedir_init(const char* pageDirectory);
 
-/**************** pagedir_save ****************/
 /* Save a webpage to a file in pageDirectory.
- * 
- * Caller provides: valid pointer to a webpage, valid pageDirectory, valid docID 
+ * Caller provides: valid pointer to a webpage, valid pageDirectory, valid docID
  * We do: write URL, depth, and HTML to pageDirectory/docID
  * We guarantee: nothing if any error opening the file
  */
 void pagedir_save(const webpage_t* page, const char* pageDirectory, const int docID);
+
+/* Verify that directory is a Crawler-produced directory.
+ * Caller provides: valid pathname string for pageDirectory
+ * We return: true if .crawler file exists, false otherwise
+ */
+bool pagedir_validate(const char* pageDirectory);
+
+/* Load a webpage from a file in pageDirectory.
+ * Caller provides: valid pageDirectory, valid docID >= 1
+ * We return: pointer to a webpage_t, or NULL if file can't be opened
+ */
+webpage_t* pagedir_load(const char* pageDirectory, const int docID);
 
 #endif // __PAGEDIR_H
